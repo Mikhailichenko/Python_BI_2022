ebd0fff05d5bbb5da747f086a1932a348b8516e7
Anastasia <anast1221@gmail.com>
2022-10-08 10:39:41 +0300

    script fastq_filtrator.py added

---
+++ homework2/fastq_filtrator.py
@@ -0,0 +1,73 @@
+#checking GC content according to conditions
+def check_gc (seq, gc_bound_min, gc_bound_max):
+    gc_content = 100 * (seq.count('G') + seq.count('C')) / len(seq)
+    if gc_content > gc_bound_min and gc_content < gc_bound_max:
+        return True
+    return False
+
+#checking lenght according to conditions
+def check_lenght (seq, length_bound_min, length_bound_max):
+    if len(seq) > length_bound_min and len(seq) < length_bound_max:
+        return True
+    return False
+
+#checking basecall qualities according to conditions
+def check_bq (seq_bq, quality_threshold):
+    mean_seq_bq = 0
+    for i in seq_bq:
+        mean_seq_bq += (ord(i) - 33) / len(seq_bq)
+    if mean_seq_bq >= quality_threshold:
+        return True
+    return False
+
+
+def main (input_fastq, output_file_prefix, gc_bounds = (0, 100), length_bounds = (0, 2**32), quality_threshold = 0, save_filtered = False):
+
+#define bounds for GC content
+    if type(gc_bounds) == type(1):
+        gc_bound_min = 0
+        gc_bound_max = gc_bounds
+    else:
+        gc_bound_min = gc_bounds[0]
+        gc_bound_max = gc_bounds[1]
+
+#define bounds for length
+    if type(length_bounds) == type(1):
+        length_bound_min = 0
+        length_bound_max = length_bounds
+    else:
+        length_bound_min = length_bounds[0]
+        length_bound_max = length_bounds[1]
+
+#open output files
+    output_file_passed = f"{output_file_prefix}_passed.fastq"
+    f_passed = open(output_file_passed, "w")
+    if save_filtered == True:
+        output_file_failed = f"{output_file_prefix}_failed.fastq"
+        f_failed = open(output_file_failed, "w")
+
+#analise fastq file by lines
+    with open(input_fastq) as file:
+        count_line = 1
+        for line in file:
+            if count_line == 1:
+                name_seq = line.strip()
+                count_line += 1
+            elif count_line == 2:
+                seq = line.strip()
+                count_line += 1
+            elif count_line == 3:
+                separator = line.strip()
+                count_line += 1
+            elif count_line == 4:
+                seq_bq = line.strip()
+                if check_gc (seq, gc_bound_min, gc_bound_max) and check_lenght (seq, length_bound_min, length_bound_max) and check_bq (seq_bq, quality_threshold):
+                    f_passed.write(f"{name_seq}\n{seq}\n{separator}\n{seq_bq}\n")
+                elif save_filtered == True:
+                    f_failed.write(f"{name_seq}\n{seq}\n{separator}\n{seq_bq}\n")
+                count_line = 1
+
+#close output files
+    f_passed.close()
+    if save_filtered == True:
+        f_failed.close()
